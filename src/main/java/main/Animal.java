204ccc967ff2186e592e286cabaf274ca74289f1
Veeti Hurskainen <veeti.hurskainen@gmail.com>
2025-01-09 22:45:52 +0200

    commit

---
+++ src/main/java/main/Animal.java
@@ -0,0 +1,13 @@
+package main;
+
+public class Animal {
+    String name;
+    String species;
+    int age;
+
+    public Animal(String name, String species, int age) {
+        this.name = "Michael";
+        this.species = "orangutan";
+        this.age = 10;
+    }
+}
